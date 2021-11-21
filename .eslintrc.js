41c4c589d346dafa0fb53924124ca4c3cbce5b48
Pierre B <pierre.bouffier@etu.univ-amu.fr>
2021-11-21 14:07:03 +0100

    Add more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    "project": "./tsconfig.json"
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  "env": {
-    "browser": true,
-    "node": true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    "rules": {
+        "@typescript-eslint/indent": ["error", 4],
+        "indent": ["error", 4],
+        "@typescript-eslint/array-type": ["error"],
+    }
 };
